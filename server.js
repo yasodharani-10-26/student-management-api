a97464ec57de4f0c50d69c2db64d82bb11eb4c83
yasodha rani <yashodarani2022014@gmail.com>
2026-05-31 12:34:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/students", (req, res) => {
-  res.json([
-    { id: 1, name: "Student A" },
-    { id: 2, name: "Student B" }
-  ]);
+  res.json([{ id: 1, name: "Student A" }]);
 });
 
 export default app;
