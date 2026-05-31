1b7d889dabe77631838a5399e735e8093d7c45e6
yasodha rani <yashodarani2022014@gmail.com>
2026-05-31 12:21:17 +0530

    Update students.js

--- students.js
+++ students.js
@@ -1,8 +1,6 @@
-import express from "express";
-const app = express();
-
-app.get("/api/students", (req, res) => {
-  res.json([{ id: 1, name: "Ravi" }]);
-});
-
-export default app;
+export default function handler(req, res) {
+  res.status(200).json([
+    { id: 1, name: "Student A" },
+    { id: 2, name: "Student B" }
+  ]);
+}
